f04848c26adc9a30d87455d0e98b8cb8c7671bb0
Benjamin Du <duchuanlong+git@gmail.com>
2017-10-16 09:30:53 -0700

    make it easy to create new users

--- init.sh
+++ init.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
+function create_user.usage() {
+    cat << EOF
+Create a new user.
+Syntax: create_user user user_id password group group_id
+
+Arguments:
+user: user name (which cannot be an empty string)
+user_id: user ID (which cannot be an empty string)
+password: password of the user (which cannot be an empty string)
+group: group name of the user (which cannot be an empty string)
+group_id: group ID (which cannot be an empty string)
+EOF
+}
+
 function create_user() {
-    if [[ $1 == "" ]]; then 
-        return 1
+    if [[ $1 == "-h" ]]; then 
+        create_user.usage
+        return 0
+    fi
+    if [[ $# != 5 ]]; then
+        create_user.usage
+        return 5
     fi
     local user=$1
     local user_id=$2
     local password=$3
     local group=$4
     local group_id=$5
+    if [[ $user == "" || $user_id == "" || $password == "" || $group == "" || $group_id == "" ]]; then 
+        create_user.usage
+        return 1
+    fi
     if [[ "$(getent group docker)" == "" ]]; then
         groupadd -o -g $group_id docker
     fi
@@ -17,9 +40,37 @@ function create_user() {
     gpasswd -a $user sudo
 }
 
-# umask 002
-# /bin/bash --login
+function create_user_nogroup.usage() {
+    cat << EOF
+Create a new user with the group name "nogroup".
+Syntax: create_user_nogroup user [password] 
 
-create_user $DOCKER_USER ${DOCKER_USER_ID:-9001} ${DOCKER_PASSWORD:-${DOCKER_USER}} ${DOCKER_GROUP:-docker} ${DOCKER_GROUP_ID:-9001}
-chmod 777 /jupyter
-/script.sh
+Arguments:
+user: user name
+password: Optional password of the user. If not provided, then the user name is used as the password.
+EOF
+}
+
+function create_user_nogroup() {
+    if [[ $1 == "-h" ]]; then
+        create_user_nogroup.usage
+        return 0
+    fi
+    if [[ $# < 1 || $# > 2 ]]; then
+        create_user_nogroup.usage
+        return 1
+    fi
+    local user=$1
+    local user_id=$(id -u $user)
+    local password=${2:-$user}
+    local group=nogroup
+    local group_id=$(getent group nogroup | cut -d: -f3)
+    create_user $user $user_id $password $group $group_id
+}
+
+
+if [[ "$0" == ${BASH_SOURCE[0]} ]]; then
+    create_user $DOCKER_USER ${DOCKER_USER_ID:-9001} ${DOCKER_PASSWORD:-${DOCKER_USER}} ${DOCKER_GROUP:-docker} ${DOCKER_GROUP_ID:-9001}
+    chmod 777 /jupyter
+    /script.sh
+fi
