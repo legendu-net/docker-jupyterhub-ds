08ac689b47a3edd8e4f21e192c9d1ec9e4f05ccd
Ben Chuanlong Du <dclong@users.noreply.github.com>
2017-10-16 22:32:58 +0800

    Update inotify.sh

--- inotify.sh
+++ inotify.sh
@@ -2,7 +2,8 @@
 
 dir=/jupyter
 inotifywait -m -r -e create --format '%w%f' "$dir" | while read f; do
-    if [[ "$f" == */.ipynb_checkpoints ]]; then
+#    if [[ "$f" == */.ipynb_checkpoints ]]; then
+    if [[ -d "$f" ]]; then
         chmod 775 "$f"
     fi
 done
