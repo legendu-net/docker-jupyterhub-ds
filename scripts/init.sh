e7dd9acb2ea02e672c7cf337094ce2d93b242f42
Ben Chuanlong Du <dclong@users.noreply.github.com>
2017-10-17 09:51:05 +0800

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/scripts/create_user $DOCKER_USER ${DOCKER_USER_ID:-9001} ${DOCKER_PASSWORD:-${DOCKER_USER}} ${DOCKER_GROUP:-docker} ${DOCKER_GROUP_ID:-9001}
+/scripts/create_user.sh $DOCKER_USER ${DOCKER_USER_ID:-9001} ${DOCKER_PASSWORD:-${DOCKER_USER}} ${DOCKER_GROUP:-docker} ${DOCKER_GROUP_ID:-9001}
 chmod 777 /jupyter
 /scripts/launch.sh
 
