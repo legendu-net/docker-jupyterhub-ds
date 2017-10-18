5a227924cb0fef1e9933b8706f38e82665073d1e
Ben Chuanlong Du <dclong@users.noreply.github.com>
2017-10-18 12:58:01 +0800

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# create a user
 /scripts/create_user.sh $DOCKER_USER ${DOCKER_USER_ID:-9001} ${DOCKER_PASSWORD:-${DOCKER_USER}} ${DOCKER_GROUP:-docker} ${DOCKER_GROUP_ID:-9001}
+# add admin user to JupyterHub
+sed -i "s/^c\.JupyterHub\.admin_users\s*=\s*set(.*)\s*$/c.JupyterHub.admin_users = set('${DOCKER_ADMIN_USER:-$DOCKER_USER}')/g" /etc/jupyter/jupyterhub_config.py 
 chmod 777 /jupyter
 /scripts/launch.sh
 
