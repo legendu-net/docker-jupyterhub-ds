6c9aba75b782e6017786b36685f9ad5f184a6edd
Ben Chuanlong Du <dclong@users.noreply.github.com>
2017-10-31 15:21:42 +0800

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # create a user
+DOCKER_USER_ID=${DOCKER_USER_ID:-9001} 
+DOCKER_PASSWORD=${DOCKER_PASSWORD:-$DOCKER_USER} 
 DOCKER_GROUP=${DOCKER_GROUP:-docker} 
-/scripts/create_user.sh $DOCKER_USER ${DOCKER_USER_ID:-9001} ${DOCKER_PASSWORD:-$DOCKER_USER} $DOCKER_GROUP ${DOCKER_GROUP_ID:-9001}
+DOCKER_GROUP_ID=${DOCKER_GROUP_ID:-9001}
+/scripts/create_user.sh $DOCKER_USER $DOCKER_USER_ID $DOCKER_PASSWORD $DOCKER_GROUP $DOCKER_GROUP_ID
 
 # add admin user to JupyterHub
 sed -i "s/^c\.JupyterHub\.admin_users\s*=\s*set(.*)\s*$/c.JupyterHub.admin_users = set('${DOCKER_ADMIN_USER:-$DOCKER_USER}')/g" /etc/jupyterhub/jupyterhub_config.py 
-
-mkdir -p "$NOTEBOOK_DIR"
-chown $USER:$DOCKER_GROUP "$NOTEBOOK_DIR"
 
 /scripts/launch.sh
 
